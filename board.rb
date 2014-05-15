1a411c8078d3106e7d97eb0921b66634cb8bf7fd
matthew short <short.matthew.f@gmail.com>
2014-05-15 09:42:13 -0400

    add init methods for board and piece.  to_s for piece.  subclasses for piece.

--- board.rb
+++ board.rb
@@ -1,3 +1,41 @@
-class Board
+class CheckerBoard
+  def self.positions
+    (0..7).map do |row|
+      (0..7).map do |col|
+        [row, col]
+      end
+    end.flatten(1)
+  end
   
+  def initialize
+    @board = setup_board
+    
+  end
+  
+  def [](pos)
+    x, y = pos
+    @board[x][y]
+  end
+  
+  def []=(pos, piece)
+    x, y = pos
+    @board[x][y] = piece
+  end
+  
+  protected
+  
+  def setup_board
+    board = Array.new(8) { Array.new(8) }
+    
+    add_pieces_to(board)
+  end
+  
+  def add_pieces_to(board)
+    (0..2).each do |row|
+      [0, 2, 4, 6].each do |col|
+        BlackPiece.new([row, col + (1 - row % 2)])
+        RedPiece.new([5 + row, col + row % 2])
+      end
+    end
+  end
 end
--- piece.rb
+++ piece.rb
@@ -1,3 +1,57 @@
 class Piece
+  def initialize(pos, board)
+    @pos = pos
+    @board = board
+    
+    @board[pos] = self        
+  end
   
+  def deltas
+    self.class::DELTAS
+  end
+  
+  def to_s
+    case self.class
+    when RedPiece
+      "r"
+    when BlackPiece
+      "b"
+    when King
+      self.color == :red ? "R" : "B"
+    end
+  end
 end
+
+class RedPiece < Piece
+  DELTAS = [
+    [-1, -1], [-1, 1]
+  ]
+  
+  def initialize(pos, board)
+    @color = :red
+    super(pos, board)
+  end
+end
+
+class BlackPiece < Piece
+  DELTAS = [
+    [1, -1], [1, 1]
+  ]  
+
+  def initialize(pos, board)
+    @color = :black
+    super(pos, board)
+  end
+end
+
+class King < Piece
+  DELTAS = [
+    [1, -1], [1, 1],
+    [-1, -1], [-1, 1]
+  ]    
+  
+  def initialize(pos, board, color)
+    @color = color
+    super(pos, board)
+  end
+end 
