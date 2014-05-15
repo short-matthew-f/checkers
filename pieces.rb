bc40d03384001f8ff975b1a86874478ba08bb92d
matthew short <short.matthew.f@gmail.com>
2014-05-15 14:10:25 -0400

    Refactor can_jump_to?, create empties_between

--- pieces.rb
+++ pieces.rb
@@ -1,20 +1,10 @@
-# require 'debugger'
+require "./checker_math.rb"
 
 class Piece
+  include CheckerMath
+  
   attr_accessor :pos
   attr_reader :board, :color
-  
-  def differential(source, target)
-    a, b = source
-    x, y = target
-    [x - a, y - b]
-  end
-  
-  def midpoint(source, target)
-    a, b = source
-    x, y = target
-    [(x + a) / 2, (y + b) / 2]
-  end
   
   def initialize(pos, board)
     @pos = pos
@@ -23,25 +13,11 @@ class Piece
     @board[pos] = self        
   end
   
-  def my_favorite_row
-    color == :red ? 0 : 7
-  end
+  protected
   
   def perform_slide(target)
     if can_slide_to?(target) 
       board[self.pos] = nil  
-      board[target] = self
-      
-      true
-    else
-      false
-    end
-  end
-  
-  def perform_jump(target)
-    if can_jump_to?(target)
-      board[self.pos] = nil
-      board[midpoint(self.pos, target)] = nil
       board[target] = self
       
       true
@@ -56,59 +32,60 @@ class Piece
     deltas.include?(vec) && board.is_empty?(target) 
   end
   
-  def can_jump_to?(target)
-    c_pos = self.pos
-    vec = differential(c_pos, target)
-    
-    tiny_vec = smallest_vec(c_pos, target)
-    
-    if deltas.include?(tiny_vec) && vec[0].even? && board.is_empty?(target)
-      only_an_enemy_halfway?(c_pos, target)
+  def perform_jump(target)
+    if can_jump_to?(target)
+      board[self.pos] = nil
+      board[midpoint(self.pos, target)] = nil
+      board[target] = self
+      
+      true
     else
       false
     end
   end
   
-  def smallest_vec(source, target)
-    vec = integral_component(differential(source, target))
+  # can_jump_to?(target) requires more logic, so we build up our checks
+  
+  def empties_between(source, target)
+    spaces = (differential(source, target)[0]).abs 
+    dir = smallest_vec(source, target)
+    
+    [].tap do |empties|
+      (1...spaces).each do |i|
+        pos = move(source, dir, i)
+        empties << pos if board.is_empty?(pos)
+      end
+    end
   end
   
-  def only_an_enemy_halfway?(source, target)
-    number_of_spaces = (differential(source, target)[0]).abs       
-    direction = integral_component(differential(self.pos, target))    
+  def only_an_enemy_halfway?(source, target)    
     middle = board[midpoint(source, target)]
     
     return false if middle.nil? || middle.color == self.color
     
-    (1...number_of_spaces).all? do |i|
-      search_spot = [source[0] + i * direction[0], source[1] + i * direction[1]]
-      if search_spot == midpoint(source, target)
-        true
-      else
-        board[search_spot].nil?
-      end
-    end
+    empties_between(source, target).count == 1
+  end
+  
+  def can_jump_to?(target)
+    c_pos = self.pos
+    vec, dir = differential(c_pos, target), smallest_vec(c_pos, target)
+  
+    deltas.include?(dir)      && 
+      vec[0].even?            && 
+      board.is_empty?(target) &&
+      only_an_enemy_halfway?(c_pos, target)
   end
 
-  def integral_component(pos)
-    x, y = pos
-    g = gcf(x, y)
-    
-    [x / g, y / g]
-  end
-  
-  def gcf(x,y)
-    (1..x.abs).map do |i|
-      ((x % i == 0) && (y % i == 0)) ? i : nil
-    end.compact.last
-  end
-  
   def deltas
     self.class::DELTAS
   end
   
   def to_s
     is_a?(King) ? "✪".colorize(color) : "◎".colorize(color)
+  end
+  
+  def my_favorite_row
+    color == :red ? 0 : 7
   end
 end
 
